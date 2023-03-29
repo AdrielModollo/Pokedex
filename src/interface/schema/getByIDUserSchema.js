ad870c8db03040d7b09eebcf797183404679e0ee
AdrielModollo <adrielmodollo@outlook.com>
2023-03-28 23:20:16 -0300

    fix: remove unnecessary getByIdUsers

--- src/interface/schema/getByIDUserSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const getByIDUserSchema = Joi.object().keys({
-    params: Joi.object().keys({
-        userId: Joi.string().required()
-    })
-});
-
-module.exports = getByIDUserSchema;
