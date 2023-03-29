895c55f71367be7dbcb534ee6f110bb1df730228
AdrielModollo <adrielmodollo@outlook.com>
2023-03-29 00:00:10 -0300

    test: user service with 100%

--- __tests__/unit/domain/services/userService.test.js
+++ __tests__/unit/domain/services/userService.test.js
@@ -1,14 +1,117 @@
 const UserService = require('../../../../src/domain/services/userService');
 const User = require('../../../../src/infra/repositories/models/userModel');
 const deleteByIDUserSchema = require('../../../../src/interface/schema/deleteByIDUserSchema');
+const updateUserSchema = require('../../../../src/interface/schema/updateUserSchema');
 
 jest.mock('../../../../src/infra/repositories/models/userModel');
 jest.mock('../../../../src/interface/schema/deleteByIDUserSchema');
+jest.mock('../../../../src/interface/schema/updateUserSchema');
 
 describe('UserService', () => {
     afterEach(() => {
         jest.resetAllMocks();
     });
+
+    describe('getUsers', () => {
+        it('should return all users', async () => {
+            const mockUsers = [{ id: 1, name: 'John Doe', email: 'johndoe@example.com', password: 'password123' }, { id: 2, name: 'Jane Doe', email: 'janedoe@example.com', password: 'password123' },];
+            User.findAll.mockResolvedValue(mockUsers);
+
+            const userService = new UserService();
+            const users = await userService.getUsers();
+
+            expect(User.findAll).toHaveBeenCalledTimes(1);
+            expect(users).toEqual(mockUsers);
+        });
+
+        it('should return an empty array if no users are found', async () => {
+            User.findAll.mockResolvedValue([]);
+
+            const userService = new UserService();
+            const users = await userService.getUsers();
+
+            expect(User.findAll).toHaveBeenCalledTimes(1);
+            expect(users).toEqual([]);
+        });
+    });
+
+    describe('updateUser', () => {
+        const userId = 1;
+        const mockUser = {
+            id: userId,
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: 'password123'
+        };
+
+        beforeEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('should throw an error if validation fails', async () => {
+            const mockError = new Error('Invalid input');
+            const mockReturnValue = { error: mockError }; // objeto que contém a propriedade error com o valor do objeto de erro
+            jest.spyOn(updateUserSchema, 'validate').mockReturnValueOnce(mockReturnValue);
+
+            const userService = new UserService();
+
+            await expect(userService.updateUser(userId, {})).rejects.toThrow('Invalid input');
+
+            expect(updateUserSchema.validate).toHaveBeenCalledTimes(1);
+            expect(updateUserSchema.validate).toHaveBeenCalledWith({ params: { userId }, body: {} });
+        });
+
+        it('should throw an error if user is not found', async () => {
+            jest.spyOn(updateUserSchema, 'validate').mockReturnValueOnce({ value: { params: { userId } }, error: undefined });
+            jest.spyOn(User, 'findByPk').mockResolvedValueOnce(null);
+
+            const userService = new UserService();
+
+            await expect(userService.updateUser(userId, {})).rejects.toThrow('User not found!');
+
+            expect(User.findByPk).toHaveBeenCalledTimes(1);
+            expect(User.findByPk).toHaveBeenCalledWith(userId);
+        });
+
+        it('should update a user', async () => {
+            const userId = '1';
+            const updatedData = {
+                name: 'Jane Doe',
+                email: 'jane.doe@example.com',
+                password: 'new_password',
+            };
+
+            const mockUser = {
+                id: 1,
+                name: 'John Doe',
+                email: 'john.doe@example.com',
+                password: 'old_password',
+                save: jest.fn().mockImplementation(() => mockUser),
+            };
+
+            User.findByPk.mockResolvedValue(mockUser);
+            updateUserSchema.validate.mockReturnValue({ error: null });
+
+            const userService = new UserService();
+            const result = await userService.updateUser(userId, updatedData);
+
+            expect(User.findByPk).toHaveBeenCalledWith(userId);
+            expect(updateUserSchema.validate).toHaveBeenCalledWith({
+                params: { userId },
+                body: updatedData,
+            });
+            expect(mockUser.save).toHaveBeenCalled();
+            expect(result).toEqual({
+                id: 1,
+                name: 'Jane Doe',
+                email: 'jane.doe@example.com',
+                password: 'new_password',
+                save: expect.any(Function),
+            });
+        });
+
+    });
+
 
     describe('getUserById', () => {
         it('should return a user by id', async () => {
